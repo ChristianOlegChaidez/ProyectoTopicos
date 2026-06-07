102d4e6933854a1f66358dcfe516b4124fee4343
Christian Olegario Chaidez Barajas <chaidez.christian@uabc.edu.mx>
2026-06-06 21:00:19 -0700

    cambios para alumnos ya funciona

---
+++ Models/Ayudantia.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProyectoTopicos.Models
+{
+    [Table("Ayudantias")]
+    public class Ayudantia
+    {
+        [Key]
+        public int Clave_Ayudantias { get; set; }
+        public string? Programa_Educativo { get; set; }
+        public int No_Programa_Educativo { get; set; }
+        public string? Plan_Estudios { get; set; }
+        public int Matricula { get; set; }
+        public string? Nombre_Alumno { get; set; }
+        public string? Nombre_de_Ayudantia { get; set; }
+        public double Creditos_de_Ayudantia { get; set; }
+        public int No_de_Empleado { get; set; }
+        public string? Nombre_Empleado { get; set; }
+    }
+}
